41e50a3d2aa4432e12540fc7b130b1bb684e3813
Ivan <Ivan.Eyukin@esplus.ru>
2023-06-11 23:50:15 +0500

    Создана база данных на SQLite. Прописано взаимодействие с БД. Настроено сохранение данных.

---
+++ src/main/java/LibBaseDto/DtoBaseUser/UserInfo.java
@@ -0,0 +1,42 @@
+package LibBaseDto.DtoBaseUser;
+
+public class UserInfo {
+
+    Long id;
+    String name;
+    String firstName;
+    String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+}
